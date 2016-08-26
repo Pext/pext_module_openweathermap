3715fa35739a8a88738f7fa08a6bd108476e2827
Sylvia van Os <iamsylvie@openmailbox.org>
2016-08-26 18:52:27 +0200

    Fix license header typo

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 # Copyright (C) 2016 Sylvia van Os <iamsylvie@openmailbox.org>
-# Pext openweathmap module is free software: you can redistribute it and/or
+#
+# Pext OpenWeatherMap module is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
