7610e05faaab6563cec4c1a30b817db539711088
Sylvia van Os <sylvia@hackerchick.me>
2020-06-16 20:09:19 +0200

    Create stable branch

--- generate_metadata.py
+++ generate_metadata.py
@@ -43,6 +43,7 @@ for language in languages:
                'homepage': 'https://pext.io/',
                'license': 'GPL-3.0+',
                'git_urls': ['https://github.com/Pext/pext_module_openweathermap'],
+               'git_branch_stable': 'stable',
                'bugtracker': 'https://github.com/Pext/pext_module_openweathermap',
                'bugtracker_type': 'github',
                'settings': [{
